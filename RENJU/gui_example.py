92b7a29026e21cbc5e901d7a607b5a489f9956bb
Oleg Desheulin <31383250+oleges1@users.noreply.github.com>
2018-06-27 03:31:29 +0300

    add comments

--- RENJU/gui_example.py
+++ RENJU/gui_example.py
@@ -12,4 +12,5 @@ Available partners:
 play = input().split('Vs')
 
 # first arg - black player, second arg - white player
+# Attention! While playing you need to click one more time to submit the chosen move.
 Runner(play[0], play[1])
